cab0bb076da56f21a84705d1fd5e52d4f214c6c2
shubham sharma <86662107+affiliatebot@users.noreply.github.com>
2025-10-03 16:43:30 +0530

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -2,16 +2,16 @@
 # Upgrade pip
 pip install --upgrade pip
 
-# Install compatible PyTorch + Torchvision + Torchaudio (CPU)
+#  Install PyTorch CPU
 pip install torch==2.1.1+cpu torchvision==0.16.1+cpu torchaudio==2.1.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
-# Install PyTorch Geometric dependencies (CPU)
+#  Install PyG dependencies (CPU)
 pip install torch-scatter==2.1.2+pt21cpu \
             torch-sparse==0.6.18+pt21cpu \
             torch-cluster==1.6.3+pt21cpu \
-            torch-spline-conv==1.3.1+pt21cpu \
+            torch-spline-conv==1.2.2+pt21cpu \
             torch-geometric==2.6.1 \
             -f https://data.pyg.org/whl/torch-2.1.1+cpu.html
 
-# Install remaining dependencies
+#  Install the rest of your dependencies
 pip install -r requirements.txt
