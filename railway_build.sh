bc12bc3684533042313d90a314ad919faaa9440a
shubham sharma <86662107+affiliatebot@users.noreply.github.com>
2025-10-04 13:46:47 +0530

    Update railway_build.sh

--- railway_build.sh
+++ railway_build.sh
@@ -19,20 +19,18 @@ python -m pip install --upgrade pip
 # --------------------------
 # Install CPU-only PyTorch + torchvision + torchaudio
 # --------------------------
-pip install torch==2.8.0 torchvision  torchaudio==2.8.0
-
+pip install torch==2.8.0+cpu torchvision torchaudio==2.8.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
 # --------------------------
 # Install PyTorch-Geometric CPU wheels
 # --------------------------
 pip install \
-  torch-scatter==2.1.2 \
-  torch-sparse==0.6.18 \
-  torch-cluster==1.6.3 \
-  torch-spline-conv==1.2.2 \
+  torch-scatter==2.1.2+cpu \
+  torch-sparse==0.6.18+cpu \
+  torch-cluster==1.6.3+cpu \
+  torch-spline-conv==1.2.2+cpu \
   torch-geometric==2.6.1 \
   -f https://data.pyg.org/whl/torch-2.8.0.html
-
 
 # --------------------------
 # Install remaining Python packages
